62f3dc6aabbe1b0936a5f19b0b28dcf95a46bc8d
Daniel Ilievskim <daniel.d.ilievski@gmail.com>
2022-12-07 15:24:54 +0100

    Added real SMTP provider

--- _helpers/send-email.js
+++ _helpers/send-email.js
@@ -1,9 +1,16 @@
-const nodemailer = require('nodemailer');
-const config = require('config.json');
+const nodemailer = require("nodemailer");
+const config = require("config.json");
 
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html, from = config.emailFrom }) {
-    const transporter = nodemailer.createTransport(config.smtpOptions);
-    await transporter.sendMail({ from, to, subject, html });
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST || "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: process.env.SMTP_USERNAME || "jake.hilpert@ethereal.email",
+      pass: process.env.SMTP_PASSWORD || "PFQwSkRnQpVrtNu53u",
+    },
+  });
+  await transporter.sendMail({ from, to, subject, html });
 }
