54f9473ee35e0df7edb1fdd17f9a94b31de17f9e
Daniel Ilievskim <daniel.d.ilievski@gmail.com>
2022-12-07 12:36:27 +0100

    Added path import for Cyclic

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const errorHandler = require("_middleware/error-handler");
+const path = require("path");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
