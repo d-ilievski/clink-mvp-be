74f76cd9f5b590b0aff8f09198e93c05283ffec5
Daniel Ilievskim <daniel.d.ilievski@gmail.com>
2023-11-28 00:32:09 +0100

    Changed names of the routes for profile for consistency

--- src/routes/profile.route.js
+++ src/routes/profile.route.js
@@ -20,20 +20,22 @@ const {
 } = require("../controllers/profile.controller");
 
 // get active profile
-router.get("/", authorize(), getActiveProfile);
+router.get("/private", authorize(), getActiveProfile);
+// get public profile
+router.get("/public/:id", getPublicProfile);
 // get all personal profiles
 router.get("/all", authorize(), getAllProfiles);
+
 // create a profile
 router.post("/create", authorize(), createProfileSchema, createProfile);
 // edit profile 
 router.put("/edit", authorize(), updateProfileSchema, updateProfile);
+
 // accepts logged in user that wants to connect
 router.post("/connect", authorize(), connectProfileSchema, connectProfile);
 // accepts anonymous user that wants to connect (lead)
 router.post("/connect-anonymous", connectAnonymousProfileSchema, connectAnonymousProfile);
 // save to contacts
 router.post("/download-contact", downloadContactSchema, downloadContact);
-// get public profile
-router.get("/:id", getPublicProfile);
 
 module.exports = router;
